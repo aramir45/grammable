82da8a0c8448e71b01e438f6ef17bd32780a108a
aramir45 <aramir33@icloud.com>
2017-12-11 06:46:59 +0000

    lesson 4.11 done

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -19,11 +19,7 @@ RSpec.describe GramsController, type: :controller do
 
 
     it "should successfully show the new form" do
-       user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+      user = FactoryBot.create(:user)
       sign_in user
 
 
@@ -37,11 +33,7 @@ RSpec.describe GramsController, type: :controller do
 
   describe "grams#create action" do
     it "should successfully create a new gram in our database" do
-       user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+      user = FactoryBot.create(:user)
       sign_in user
 
 
@@ -54,12 +46,8 @@ RSpec.describe GramsController, type: :controller do
     end
 
   it "should properly deal with validation errors" do
-     user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
-      sign_in user
+    user = FactoryBot.create(:user)
+    sign_in user
 
 
     gram_count = Gram.count
